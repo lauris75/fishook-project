38a2f713181938cd10fae6ee76c98be64eb64bf4
Laurynas Plokštys <laurynas.plok5@gmail.com>
2025-05-19 19:19:36 +0300

    doesnt work with .env sadly

--- backend/fishook/src/main/java/com/fishook/fishook/FishookApplication.java
+++ backend/fishook/src/main/java/com/fishook/fishook/FishookApplication.java
@@ -1,14 +1,26 @@
 package com.fishook.fishook;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FishookApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(FishookApplication.class);
+
+    @Value("${SERVER_PORT:8080}")
+    private String serverPort;
+
     public static void main(String[] args) {
         SpringApplication.run(FishookApplication.class, args);
     }
 
-
+    @PostConstruct
+    public void init() {
+        logger.info("SERVER_PORT from environment: {}", serverPort);
+    }
 }
--- backend/fishook/src/main/java/com/fishook/fishook/config/JwtService.java
+++ backend/fishook/src/main/java/com/fishook/fishook/config/JwtService.java
@@ -5,7 +5,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -18,8 +17,7 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-    @Value("${JWT_SECRET_KEY}")
-    private String secretKey;
+    private static String secretKey = "7234753778214125442A472D4B6150645367566B59703373367638792F423F4528482B4D6251655468576D5A7134743777217A24432646294A404E635266556A";
 
     public String extractEmail(String token) {
         return extractClaim(token, Claims::getSubject);
